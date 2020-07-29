3d8e7e767369b12943de53fcab9ad66d1c0671fe
Dawid Ulfik <dawulf97@gmail.com>
2020-07-29 23:46:22 +0200

    Exception handler for UsernameAlreadyExistsException added in CustomResponseEntityExceptionHandler

--- mentoring/src/main/java/com/javasolution/app/mentoring/exceptions/CustomResponseEntityExceptionHandler.java
+++ mentoring/src/main/java/com/javasolution/app/mentoring/exceptions/CustomResponseEntityExceptionHandler.java
@@ -1,9 +1,20 @@
 package com.javasolution.app.mentoring.exceptions;
 
+import com.javasolution.app.mentoring.responses.UsernameAlreadyExistsResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 //@RestController
 public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleUsernameAlreadyExists(UsernameAlreadyExistsException ex, WebRequest request){
+        final UsernameAlreadyExistsResponse exceptionResponse= new UsernameAlreadyExistsResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 }
