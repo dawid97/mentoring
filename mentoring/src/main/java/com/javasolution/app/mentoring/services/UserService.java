35d26d7d1331b61d0ad2a9a5b68c71fa8646bca4
Dawid Ulfik <dawulf97@gmail.com>
2020-07-29 23:05:41 +0200

    signUpUser method in UserService added

--- mentoring/src/main/java/com/javasolution/app/mentoring/services/UserService.java
+++ mentoring/src/main/java/com/javasolution/app/mentoring/services/UserService.java
@@ -1,10 +1,13 @@
 package com.javasolution.app.mentoring.services;
 
+import com.javasolution.app.mentoring.entities.ConfirmationToken;
 import com.javasolution.app.mentoring.entities.User;
 import com.javasolution.app.mentoring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,15 +15,29 @@ import java.util.Optional;
 @Service
 public class UserService implements UserDetailsService {
 
+    @Autowired //must be autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
-        this.userRepository=userRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         final Optional<User> optionalUser = userRepository.findByEmail(email);
-        return optionalUser.orElseThrow(() -> new UsernameNotFoundException("User with email "+email+" cannot be found."));
+        return optionalUser.orElseThrow(() -> new UsernameNotFoundException("User with email " + email + " cannot be found."));
+    }
+
+    public User signUpUser(User user) {
+        final String encryptedPassword = bCryptPasswordEncoder.encode(user.getPassword());
+        user.setPassword(encryptedPassword);
+        user.setConfirmPassword("");
+
+        final User savedUser = userRepository.save(user);
+
+        savedUser.setPassword("");
+        return savedUser;
     }
 }
