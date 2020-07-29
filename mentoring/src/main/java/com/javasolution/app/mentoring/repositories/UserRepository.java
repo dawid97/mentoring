e5fe2ed2417bdf03153c78c8ed63f2c3bb653889
Dawid Ulfik <dawulf97@gmail.com>
2020-07-29 21:41:53 +0200

    UserRepository added

---
+++ mentoring/src/main/java/com/javasolution/app/mentoring/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.javasolution.app.mentoring.repositories;
+
+import com.javasolution.app.mentoring.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+}
