619858c3ef49af902662729176173ff4bbbc5417
Dawid Ulfik <dawulf97@gmail.com>
2020-07-29 23:41:48 +0200

    CustomResponseEntityExceptionHandler created

---
+++ mentoring/src/main/java/com/javasolution/app/mentoring/exceptions/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.javasolution.app.mentoring.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+//@RestController
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+}
