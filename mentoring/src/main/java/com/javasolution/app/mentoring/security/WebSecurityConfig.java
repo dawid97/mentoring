73b14d4c4c9213b387961774e4425357016fed29
Dawid Ulfik <dawulf97@gmail.com>
2020-07-29 22:57:20 +0200

    WebSecurityConfig and BCryptPasswordEncoder bean added

---
+++ mentoring/src/main/java/com/javasolution/app/mentoring/security/WebSecurityConfig.java
@@ -0,0 +1,43 @@
+package com.javasolution.app.mentoring.security;
+
+
+import com.javasolution.app.mentoring.services.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
+@EnableWebSecurity
+@AllArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/users/sign-in", "/api/users/sign-up/**").permitAll()
+                .anyRequest()
+                .authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(bCryptPasswordEncoder());
+    }
+}
