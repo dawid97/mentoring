45f51992911ab3c3d54aef9121cee425fbf4d85e
Dawid Ulfik <dawulf97@gmail.com>
2020-07-30 13:29:15 +0200

    authenticateUser endpoint added in UserController

--- mentoring/src/main/java/com/javasolution/app/mentoring/controllers/UserController.java
+++ mentoring/src/main/java/com/javasolution/app/mentoring/controllers/UserController.java
@@ -2,6 +2,9 @@ package com.javasolution.app.mentoring.controllers;
 
 import com.javasolution.app.mentoring.entities.ConfirmationToken;
 import com.javasolution.app.mentoring.entities.User;
+import com.javasolution.app.mentoring.requests.LoginRequest;
+import com.javasolution.app.mentoring.responses.LoginResponse;
+import com.javasolution.app.mentoring.security.JwtUtil;
 import com.javasolution.app.mentoring.services.ConfirmationTokenService;
 import com.javasolution.app.mentoring.services.MapValidationErrorService;
 import com.javasolution.app.mentoring.services.UserService;
@@ -9,6 +12,9 @@ import com.javasolution.app.mentoring.validators.UserValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,6 +30,27 @@ public class UserController {
     private final ConfirmationTokenService confirmationTokenService;
     private final UserValidator userValidator;
     private final MapValidationErrorService mapValidationErrorService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) throws Exception {
+
+        final ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+
+        if (errorMap != null) return errorMap;
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getEmail(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        final UserDetails userDetails = userService.loadUserByUsername(loginRequest.getEmail());
+        final String jwt = jwtUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new LoginResponse(jwt));
+    }
 
     @GetMapping("/sign-up/confirm")
     ResponseEntity<?> confirmMail(@RequestParam("token") String token) {
@@ -36,10 +63,10 @@ public class UserController {
     @PostMapping("/sign-up")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result) {
 
-        userValidator.validate(user,result);
-        final ResponseEntity<?> errorMap= mapValidationErrorService.mapValidationService(result);
+        userValidator.validate(user, result);
+        final ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
 
-        if(errorMap!=null) return errorMap;
+        if (errorMap != null) return errorMap;
 
         final User newUser = userService.signUpUser(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
