24b6f02a4d2ce2e9b31eda7ae83d7a1d18b69b36
Dawid Ulfik <dawulf97@gmail.com>
2020-07-30 13:14:06 +0200

    createToken method added in JwtUtil

--- mentoring/src/main/java/com/javasolution/app/mentoring/security/JwtUtil.java
+++ mentoring/src/main/java/com/javasolution/app/mentoring/security/JwtUtil.java
@@ -2,9 +2,11 @@ package com.javasolution.app.mentoring.security;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
 
@@ -28,5 +30,11 @@ public class JwtUtil {
 
     private Boolean isTokenExpired(String token){
         return extractExpiration(token).before(new Date());
+    }
+
+    private String createToken(Map<String,Object> claims, String subject){
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256,SECRET_KEY).compact();
     }
 }
