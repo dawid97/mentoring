e5f99acd41c34691c067981aff820b7f97fee244
Dawid Ulfik <dawulf97@gmail.com>
2020-07-30 13:05:18 +0200

    extractAllClaims method added in JwtUtil

--- mentoring/src/main/java/com/javasolution/app/mentoring/security/JwtUtil.java
+++ mentoring/src/main/java/com/javasolution/app/mentoring/security/JwtUtil.java
@@ -1,8 +1,16 @@
 package com.javasolution.app.mentoring.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class JwtUtil {
+
+    private String SECRET_KEY = "secret";
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+    }
 }
