186ee95654aef39bd0d67f5ea8c17de0f8667082
Dawid Ulfik <dawulf97@gmail.com>
2020-07-30 13:22:58 +0200

    LoginResponse created

---
+++ mentoring/src/main/java/com/javasolution/app/mentoring/responses/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.javasolution.app.mentoring.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String jwt;
+}
