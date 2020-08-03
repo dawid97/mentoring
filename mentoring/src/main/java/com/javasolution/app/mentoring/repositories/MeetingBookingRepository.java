91c6e654ffbf28be85ed1f524e4974e5aa76c9f8
Dawid Ulfik <dawulf97@gmail.com>
2020-08-03 23:15:17 +0200

    MeetingBookingRepository created

---
+++ mentoring/src/main/java/com/javasolution/app/mentoring/repositories/MeetingBookingRepository.java
@@ -0,0 +1,13 @@
+package com.javasolution.app.mentoring.repositories;
+
+import com.javasolution.app.mentoring.entities.MeetingBooking;
+import com.javasolution.app.mentoring.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MeetingBookingRepository extends CrudRepository<MeetingBooking, Long> {
+}
