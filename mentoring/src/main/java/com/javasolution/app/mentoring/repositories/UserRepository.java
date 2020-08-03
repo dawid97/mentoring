0136dc2b34443879886deafe22f456c49b86414e
Dawid Ulfik <dawulf97@gmail.com>
2020-08-03 21:13:39 +0200

    Returned type in findByEmail method changed

--- mentoring/src/main/java/com/javasolution/app/mentoring/repositories/UserRepository.java
+++ mentoring/src/main/java/com/javasolution/app/mentoring/repositories/UserRepository.java
@@ -4,10 +4,7 @@ import com.javasolution.app.mentoring.entities.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+    User findByEmail(String email);
 }
