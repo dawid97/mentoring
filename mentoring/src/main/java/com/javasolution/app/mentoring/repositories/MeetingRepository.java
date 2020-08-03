c4954ab49679c407a457062999a329cd68915b10
Dawid Ulfik <dawulf97@gmail.com>
2020-08-03 23:11:43 +0200

    Bug fixes in MeetingRepository

--- mentoring/src/main/java/com/javasolution/app/mentoring/repositories/MeetingRepository.java
+++ mentoring/src/main/java/com/javasolution/app/mentoring/repositories/MeetingRepository.java
@@ -1,3 +1,9 @@
 package com.javasolution.app.mentoring.repositories;
 
+import com.javasolution.app.mentoring.entities.Meeting;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface MeetingRepository extends CrudRepository<Meeting, Long> {
+}
