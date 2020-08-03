22161b830e30f44399cdae23da4bc16c4316779c
Dawid Ulfik <dawulf97@gmail.com>
2020-08-03 23:24:56 +0200

    UserNotFoundResponse created

---
+++ mentoring/src/main/java/com/javasolution/app/mentoring/responses/UserNotFoundResponse.java
@@ -0,0 +1,14 @@
+package com.javasolution.app.mentoring.responses;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UserNotFoundResponse {
+
+    private String user;
+}
