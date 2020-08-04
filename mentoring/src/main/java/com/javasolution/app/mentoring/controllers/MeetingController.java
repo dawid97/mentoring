9f4a0e72aad1b31de2130293f721e25586245bcc
Dawid Ulfik <dawulf97@gmail.com>
2020-08-04 15:53:48 +0200

    MeetingController created

---
+++ mentoring/src/main/java/com/javasolution/app/mentoring/controllers/MeetingController.java
@@ -0,0 +1,11 @@
+package com.javasolution.app.mentoring.controllers;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/meetings")
+public class MeetingController {
+}
