d55ab8d482b6d172083a1867903a4913ae12dbe1
Dawid Ulfik <dawulf97@gmail.com>
2020-08-04 17:53:14 +0200

    MeetingBookingService created

---
+++ mentoring/src/main/java/com/javasolution/app/mentoring/services/MeetingBookingService.java
@@ -0,0 +1,10 @@
+package com.javasolution.app.mentoring.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@AllArgsConstructor
+public class MeetingBookingService {
+}
