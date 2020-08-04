22aee71aecf519042cb2b83a4f06fd64ac232a22
Dawid Ulfik <dawulf97@gmail.com>
2020-08-04 17:13:46 +0200

    MeetingNotFoundResponse created

---
+++ mentoring/src/main/java/com/javasolution/app/mentoring/responses/MeetingNotFoundResponse.java
@@ -0,0 +1,13 @@
+package com.javasolution.app.mentoring.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class MeetingNotFoundResponse {
+
+    private String meeting;
+}
