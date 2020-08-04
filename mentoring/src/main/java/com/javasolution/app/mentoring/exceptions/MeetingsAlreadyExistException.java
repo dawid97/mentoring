7287c75ce3c7c1cbbda6590df1a19914055f046c
Dawid Ulfik <dawulf97@gmail.com>
2020-08-04 16:08:21 +0200

    MeetingsAlreadyExistException created

---
+++ mentoring/src/main/java/com/javasolution/app/mentoring/exceptions/MeetingsAlreadyExistException.java
@@ -0,0 +1,47 @@
+package com.javasolution.app.mentoring.exceptions;
+
+import com.javasolution.app.mentoring.entities.Meeting;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+@Getter
+@Setter
+public class MeetingsAlreadyExistException extends RuntimeException {
+
+    List<MeetingModel> collisionMeetings;
+
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    private class MeetingModel {
+        private LocalDate meetingDate;
+        private LocalTime meetingStartTime;
+        private LocalTime meetingEndTime;
+    }
+
+    public MeetingsAlreadyExistException(String message, List<Meeting> collisionMeetings) {
+        super(message);
+
+        this.collisionMeetings = new ArrayList<>();
+
+        for (Meeting collisionMeeting : collisionMeetings) {
+
+            this.collisionMeetings.add(
+                    new MeetingModel(
+                            collisionMeeting.getMeetingDate(),
+                            collisionMeeting.getMeetingStartTime(),
+                            collisionMeeting.getMeetingEndTime()
+                    )
+            );
+        }
+    }
+}
