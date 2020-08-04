2c883b4a7fa9d44669bd4a259090b5dc28578193
Dawid Ulfik <dawulf97@gmail.com>
2020-08-04 16:01:44 +0200

    findUserByRole method added in UserRepository

--- mentoring/src/main/java/com/javasolution/app/mentoring/repositories/UserRepository.java
+++ mentoring/src/main/java/com/javasolution/app/mentoring/repositories/UserRepository.java
@@ -1,10 +1,13 @@
 package com.javasolution.app.mentoring.repositories;
 
 import com.javasolution.app.mentoring.entities.User;
+import com.javasolution.app.mentoring.entities.UserRole;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByEmail(String email);
+
+    User findByUserRole(UserRole userRole);
 }
