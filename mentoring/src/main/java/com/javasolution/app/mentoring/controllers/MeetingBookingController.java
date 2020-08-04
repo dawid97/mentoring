eedfd086eeb4b8417695e2d8f0ae6e8c709e756f
Dawid Ulfik <dawulf97@gmail.com>
2020-08-04 17:53:41 +0200

    MeetingBookingController created

---
+++ mentoring/src/main/java/com/javasolution/app/mentoring/controllers/MeetingBookingController.java
@@ -0,0 +1,12 @@
+package com.javasolution.app.mentoring.controllers;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api")
+public class MeetingBookingController {
+}
