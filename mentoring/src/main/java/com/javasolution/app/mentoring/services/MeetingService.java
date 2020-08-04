73b53e3d0f51d63162201f6726b6ce9e80da7509
Dawid Ulfik <dawulf97@gmail.com>
2020-08-04 16:00:43 +0200

    checkCollision method added in MeetingService

--- mentoring/src/main/java/com/javasolution/app/mentoring/services/MeetingService.java
+++ mentoring/src/main/java/com/javasolution/app/mentoring/services/MeetingService.java
@@ -1,11 +1,33 @@
 package com.javasolution.app.mentoring.services;
 
 
+import com.javasolution.app.mentoring.entities.Meeting;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
 @AllArgsConstructor
 public class MeetingService {
+
+    private List<Meeting> checkCollision(List<Meeting> databaseMeetings, List<Meeting> meetings) {
+        final List<Meeting> collisionMeetings = new ArrayList<>();
+
+        for (Meeting meeting : meetings) {
+            for (Meeting databaseMeeting : databaseMeetings) {
+
+                if (meeting.getMeetingStartTime().equals(databaseMeeting.getMeetingStartTime())
+                        && meeting.getMeetingEndTime().equals(databaseMeeting.getMeetingEndTime())
+                        && meeting.getMeetingDate().equals(databaseMeeting.getMeetingDate())) {
+                    collisionMeetings.add(meeting);
+                }
+            }
+        }
+
+        return collisionMeetings;
+    }
 }
